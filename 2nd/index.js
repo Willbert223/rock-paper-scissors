41783cc1135ddd31b3ab1a7af2fc46d5024f6894
willbert978 <wcameron2003@gmail.com>
2025-04-22 01:33:37 -0500

    Did the psuedocode/planning for the whole thing

--- 2nd/index.js
+++ 2nd/index.js
@@ -1,1 +1,21 @@
-console.log('hello world!')
+//score count
+
+// When the computer generates number
+//  choose a number randomly that returns one string
+
+// ask player for input
+// refuse invalid input
+
+
+// when player types input it can be AnY size and still work
+// if value is < value print you win
+// if value is > value print you lose
+// if value is < value print you win
+// if value is > value print you lose
+// if value is < value print you win
+// if value is > value print you lose
+ 
+// use above code in this block 
+// call function 5 times
+
+
