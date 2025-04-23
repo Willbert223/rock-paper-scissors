09b90d2b5d273dc16fa953b48e4adc6109f704cd
willbert978 <wcameron2003@gmail.com>
2025-04-23 01:26:49 -0500

    Got the humanchoice prompt working and made it work with any size letter capitalt or lowercase

--- 2nd/index.js
+++ 2nd/index.js
@@ -20,10 +20,26 @@ console.log(getComputerChoice())
 
 
 
-// ask player for input
-// refuse invalid input
+// ask the player for their input
+    function getHumanChoice() {
+        let answer
+        answer = window.prompt('Enter rock, paper or scissors').toLowerCase()
 
+        if (answer === 'rock' || answer === 'paper' || answer ==='scissors') {
+            return answer
+        } 
+        // refuse invalid input
+        else if (answer !== 'rock' || answer !== 'paper' || answer !=='scissors') {
+            return 'Invalid Input'
+        }
+        
+        
 
+    }
+
+console.log(getHumanChoice())
+
+function playRound(getHumanChoice, computerChoice) {
 // when player types input it can be AnY size and still work
 // if value is < value print you win
 // if value is > value print you lose
@@ -31,6 +47,13 @@ console.log(getComputerChoice())
 // if value is > value print you lose
 // if value is < value print you win
 // if value is > value print you lose
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
+
  
 // use above code in this block 
 // call function 5 times
