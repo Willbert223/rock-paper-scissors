99b2c20c00cf62d943130bd5f9145ffde7ca124d
willbert978 <wcameron2003@gmail.com>
2025-04-24 02:19:22 -0500

    Finally got the increment working sorta. had to look it up on google though could not figure out those two lines myself.

--- 2nd/index.js
+++ 2nd/index.js
@@ -16,14 +16,14 @@ var num = Math.random()
 
 }
 
-console.log(getComputerChoice())
+
 
 
 
 // ask the player for their input
     function getHumanChoice() {
         let answer
-        answer = window.prompt('Enter rock, paper or scissors').toLowerCase()
+        answer = window.prompt('Enter rock, paper or scissors').toLowerCase()// when player types input it can be AnY size and still work
 
         if (answer === 'rock' || answer === 'paper' || answer ==='scissors') {
             return answer
@@ -37,22 +37,57 @@ console.log(getComputerChoice())
 
     }
 
-console.log(getHumanChoice())
 
-function playRound(getHumanChoice, computerChoice) {
-// when player types input it can be AnY size and still work
+
+function playRound(getHumanChoice, getComputerChoice) {
 // if value is < value print you win
+if (getHumanChoice === 'rock' && getComputerChoice === 'scissors') {
+    humanScore++
+    humanScore.textContent = humanScore;
+    console.log("player score: ", humanScore);
+    console.log('Player wins, rocks crush scissors') 
+    
+
+} else if (getHumanChoice === 'paper' && getComputerChoice  === 'rock') {
+    humanScore++
+    humanScore.textContent = humanScore;
+    console.log("player score: ", humanScore);
+    console.log('Player wins, paper covers rocks')
+    
+} else if (getHumanChoice === 'scissors' && getComputerChoice  === 'paper') {
+    humanScore++
+    humanScore.textContent = humanScore;
+    console.log("player score: ", humanScore);
+    console.log('Player wins, scissors cut paper')
+}
+// if value = value its a tie
+else if (getHumanChoice === 'paper' && getComputerChoice  === 'paper') {
+    console.log('You tied, paper')
+}else if (getHumanChoice === 'rock' && getComputerChoice  === 'rock') {
+    console.log('You tied, rock')
+}else if (getHumanChoice === 'scissors' && getComputerChoice === 'scissors') {
+    console.log('You tied, scissors')
+}
+
 // if value is > value print you lose
-// if value is < value print you win
-// if value is > value print you lose
-// if value is < value print you win
-// if value is > value print you lose
+else {
+    computerScore++;
+    computerScore.textContent = computerScore;
+    console.log("computer score: ", computerScore);
+    console.log('Computer wins')
+    
+}
+
+
+
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection)
+playRound(humanSelection, computerSelection,)
+
+
 
  
 // use above code in this block 
